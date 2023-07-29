9f8a7d53c7cd6f424f0b7f4d40025ef0e3eff016
faizal108 <69467378+faizal108@users.noreply.github.com>
2023-07-29 06:16:26 +0530

    Little Update

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <div className="app-container">
-     <h1>Pokemon Kingdom .</h1>
+     <h1>Pokemon Kingdom</h1>
     
      <div className="pokemon-container">
        <div className="all-container">
